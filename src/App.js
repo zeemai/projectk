185e37276cf0bff1cccd4f249116cba5a7c87031
apple <apple@zihaos-air.mynet>
2019-08-18 21:10:22 +0100

    Increase the text size

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yes?
+          <h1>Project K</h1>
         </p>
         <a
           className="App-link"
