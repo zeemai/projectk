fa27b42ac3577f286831fab68fde3d4b0b814932
apple <apple@zihaos-air.mynet>
2019-10-05 19:12:28 +0100

    #19 Return a list of shows from the backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,6 +2,8 @@ const express = require ('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const getShows = require('./shows')
+
+app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/backend/shows.js
@@ -0,0 +1,27 @@
+module.exports = function getShows () {
+    return [{ 
+            id: 'inception', 
+            name: 'Inception', 
+            synopsis: 'Inception is a 2010 science fiction action film written and directed by Christopher Nolan, who also produced the film with his wife, Emma Thomas.' 
+          },
+          { id: 'deepwaterHorizon', 
+            name: 'Deepwater Horizon', 
+            synopsis: 'Deepwater Horizon is a 2016 American disaster film based on the Deepwater Horizon explosion and oil spill in the Gulf of Mexico.'
+          },
+          { id: 'kong', 
+            name: 'Kong', 
+            synopsis: 'Kong is a 2017 American monster film directed by Jordan Vogt-Roberts and written by Dan Gilroy, Max Borenstein and Derek Connolly.'
+          },
+          { id: 'pulpFiction', 
+            name: 'Pulp Fiction', 
+            synopsis: 'Pulp Fiction is a 1994 American crime film written and directed by Quentin Tarantino, who conceived it with Roger Avary. Starring John Travolta, Samuel L. Jackson, Bruce Willis, Tim Roth, Ving Rhames, and Uma Thurman, it tells several stories of criminal Los Angeles.'
+          },
+          { id: 'starTrek', 
+            name: 'Star Trek', 
+            synopsis: 'Star Trek is a 2009 American science fiction film directed by J. J. Abrams and written by Roberto Orci and Alex Kurtzman.'
+          },
+          { id: 'theMist', 
+            name: 'The Mist', 
+            synopsis: 'The Mist is a 2007 American science fiction horror film based on the 1980 novella The Mist by Stephen King.'
+          }]
+}
